8b6cc86f8f9ae3c2d5a05cbe3876752544247880
eunji hwang <eunjihwang@uno-solution.com>
2021-03-14 00:49:27 +0900

    update: 테스트코드를 작성했는데 안됨. 어디서부터 잘못된 것?" "

--- src/main/java/com/springboot/book/chapter02/Application.java
+++ src/main/java/com/springboot/book/chapter02/Application.java
@@ -2,9 +2,10 @@ package com.springboot.book.chapter02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// @SpringBootApplication 이하 부터 설정을 읽어나가기 때문에 최상단 클래스에 어노테이션 추가
-@SpringBootApplication // 스프링부트 프래임워크의 최상단을 나타냄
+@EnableJpaAuditing // JPA auditing 활성화
+@SpringBootApplication // 스프링부트 프래임워크의 최상단을 나타냄 @SpringBootApplication 이하 부터 설정을 읽어나가기 때문에 최상단 클래스에 어노테이션 추가
 public class Application { // 메인 클래스(최상단클래스)
   public static void main(String[] args) { // 메인함수를 담는다
     // SpringApplication.run 으로 내장 WAS(web application server) 실행
